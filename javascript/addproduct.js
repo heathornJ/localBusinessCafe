4813ccbb37048b4c40c990859bcf4f1c7bf0967b
JetRhino <heathornj@gmail.com>
2024-06-25 18:06:48 +0100

    Add working JS for addproduct.js and tweaks to product page structure.

--- javascript/addproduct.js
+++ javascript/addproduct.js
@@ -14,10 +14,27 @@ function addMenuItem(menuItem){
 
     //Sets the content of the li element
     articleItem.innerHTML = `
-        <img src= {graphic}>
+        <img src="${menuItem.graphic}">
               <div class="product-text">
-                <h3>itemName}</h2>
-                <p>{description}</p>
+                <h3>${menuItem.itemName}</h2>
+                <p>${menuItem.description}</p>
               </div>
     `;
+
+    if (menuItem.type === 'food'){
+        document.querySelector('#product-food .product-section').appendChild(articleItem);
+    } else if (menuItem.type === 'drink'){
+        document.querySelector('#product-drink .product-section').appendChild(articleItem);
+    } else if (menuItem.type === 'apparel'){
+        document.querySelector('#product-apparel .product-section').appendChild(articleItem);
+    }
 }
+
+//Food products
+addMenuItem(productItemFactory('food', 'Cake', 'A nice slice of cake.', '../images/logonotext.png'));
+
+//Drink products
+addMenuItem(productItemFactory('drink', 'Coffee Beans', 'Our famous blend.', '../images/logonotext.png'));
+
+//Apparel products
+addMenuItem(productItemFactory('apparel', 'T-Shirt', 'The Local Business Cafe branded T-Shirt.', '../images/logonotext.png'));
