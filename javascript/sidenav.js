253f1f9e35d3ae3b0ca80b9121160cd8457c8762
JetRhino <heathornj@gmail.com>
2024-06-27 18:03:15 +0100

    Add JS to ensure sidebar menus display and hide at appropriate times, a section to home to house important information, and style rules to support this.

--- javascript/sidenav.js
+++ javascript/sidenav.js
@@ -1,18 +1,22 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("openbtn").style.opacity = "0";
 }
 
 // Close/hide the sidenav
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("openbtn").style.opacity = "100%";
 }
 
 //Checks for window resize and makes sure width is reset when adjusted
 window.addEventListener("resize", function () {
   var nav = document.getElementById("mySidenav");
+  var openButtton = document.getElementById("openbtn");
   if (window.innerWidth >= 769) {
     nav.style.width = "100vw"; // Ensure width is 100vw for larger screens
   } else {
     nav.style.width = "0"; // Reset to 0 for smaller screens
+    openButtton.style.opacity = "100%";
   }
 });
