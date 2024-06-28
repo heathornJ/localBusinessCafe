9ebec943aa43f9a2cb0e95b86d370e1e198f94b3
JetRhino <heathornj@gmail.com>
2024-06-28 11:38:04 +0100

    Add translucent background to background sections for readability, changed nav width to 100% from 100vw to fix content overflow issue, added new images to About page, and adjusted form size on mobile.

--- javascript/sidenav.js
+++ javascript/sidenav.js
@@ -14,7 +14,7 @@ window.addEventListener("resize", function () {
   var nav = document.getElementById("mySidenav");
   var openButtton = document.getElementById("openbtn");
   if (window.innerWidth >= 769) {
-    nav.style.width = "100vw"; // Ensure width is 100vw for larger screens
+    nav.style.width = "100%"; // Ensure width is 100vw for larger screens
   } else {
     nav.style.width = "0"; // Reset to 0 for smaller screens
     openButtton.style.opacity = "100%";
