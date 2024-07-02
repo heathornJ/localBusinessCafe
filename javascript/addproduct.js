391e20b441a75d8d0e78d167962db7a0aff653aa
JetRhino <heathornj@gmail.com>
2024-07-02 16:39:59 +0100

    Add compacted css, alt tags to img elements and addproduct.js, aria-label added to anchor tags without text, page descriptions, and changed img types to webp.

--- javascript/addproduct.js
+++ javascript/addproduct.js
@@ -1,9 +1,10 @@
-const product = (type, itemName, description, graphic) => {
+const product = (type, itemName, description, graphic, altText) => {
   return {
     type,
     itemName,
     description,
     graphic,
+    altText,
   };
 };
 
@@ -14,7 +15,7 @@ function addItem(product) {
 
   //Sets the content of the li element
   articleItem.innerHTML = `
-        <img src="${product.graphic}">
+        <img src="${product.graphic}" alt="${product.altText}">
               <div class="product-text">
                 <h3>${product.itemName}</h2>
                 <p>${product.description}</p>
@@ -46,16 +47,23 @@ const coffeeProducts = [
     c,
     "Coffee Beans - 500g",
     "Our famous blend, available for you to take home.",
-    "../images/coffeebag.png"
+    "../images/coffeebag.webp",
+    "A brown paper bag surounded buy unground coffee beans."
   ),
-  product(c,
-  "Coffee Beans - 1kg",
-  "Our famous blend, available for you to take home.",
-  "../images/coffeebag.png"),
-  product(c,
-  "Coffee Beans - 2kg",
-  "Our famous blend, available for you to take home.",
-  "../images/coffeebag.png")
+  product(
+    c,
+    "Coffee Beans - 1kg",
+    "Our famous blend, available for you to take home.",
+    "../images/coffeebag.webp",
+    "A brown paper bag surounded buy unground coffee beans."
+  ),
+  product(
+    c,
+    "Coffee Beans - 2kg",
+    "Our famous blend, available for you to take home.",
+    "../images/coffeebag.webp",
+    "A brown paper bag surounded buy unground coffee beans."
+  ),
 ];
 
 const merchandiseProducts = [
@@ -63,16 +71,23 @@ const merchandiseProducts = [
     m,
     "T-Shirt",
     "The Local Business Cafe branded T-Shirt.",
-    "../images/shirt.png"
+    "../images/shirt.webp",
+    "A t-shirt with the Local Business Café logo on chest."
   ),
-  product(m,
-  "Hoodie",
-  "The Local Business Cafe branded hoodie.",
-  "../images/hoodie.png"),
-  product(m,
-  "Cap",
-  "The Local Business Cafe branded cap.",
-  "../images/cap.png")
+  product(
+    m,
+    "Hoodie",
+    "The Local Business Cafe branded hoodie.",
+    "../images/hoodie.webp",
+    "A hoodie with the Local Business Café logo on chest."
+  ),
+  product(
+    m,
+    "Cap",
+    "The Local Business Cafe branded cap.",
+    "../images/cap.webp",
+    "A cap with the Local Business Café logo on chest."
+  ),
 ];
 
 const giftProducts = [
@@ -80,12 +95,16 @@ const giftProducts = [
     g,
     "Gift Card",
     "Nothing says 'Thank you', 'I love you', or 'Good job!' like free coffee. Give the gift of coffee with our gift card.",
-    "../images/giftcard.png"
+    "../images/giftcard.webp",
+    "A gift card with the Local Business Café logo on the front."
   ),
-  product(g,
-  "Gift Set",
-  "Give the gift of coffee with a thermos, 500g of coffee.",
-  "../images/giftset.png"),
+  product(
+    g,
+    "Gift Set",
+    "Give the gift of coffee with a thermos, 500g of coffee.",
+    "../images/giftset.webp",
+    "A coffee thermos and coffee pouch both with the Local Business Café logo."
+  ),
 ];
 
 coffeeProducts.forEach(addItem);
